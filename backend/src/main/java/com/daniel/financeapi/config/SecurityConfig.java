958c48c58ee0f82cc7eb3ddca83618a1fbe786a1
Dan-2000 <81095977+Dan-2000@users.noreply.github.com>
2026-03-31 18:44:34 +0000

    Fix to requests in the SecurityConfig

--- backend/src/main/java/com/daniel/financeapi/config/SecurityConfig.java
+++ backend/src/main/java/com/daniel/financeapi/config/SecurityConfig.java
@@ -1,12 +1,13 @@
 package com.daniel.financeapi.config;
-import com.daniel.financeapi.security.JwtAuthFilter;
-import com.daniel.financeapi.security.JwtUtil;
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import com.daniel.financeapi.security.JwtAuthFilter;
+import com.daniel.financeapi.security.JwtUtil;
 
 @Configuration
 public class SecurityConfig {
@@ -18,7 +19,7 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         JwtAuthFilter jwtAuthFilter = new JwtAuthFilter(jwtUtil);
-        http.addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class).csrf(csrf -> csrf.disable()).authorizeHttpRequests(auth -> auth.requestMatchers("/register", "/login").permitAll().anyRequest().authenticated());
+        http.addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class).csrf(csrf -> csrf.disable()).authorizeHttpRequests(auth -> auth.requestMatchers(HttpMethod.OPTIONS, "/**").permitAll().requestMatchers("/register", "/login").permitAll().anyRequest().authenticated());
         return http.build();
     }
 }
